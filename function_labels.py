e8bf7acbc34f79a624aeec8f742d04d320e19991
WeiW389 <w.wei.music@gmail.com>
2026-03-03 01:31:35 -0500

    Create function_labels.py

---
+++ function_labels.py
@@ -0,0 +1,90 @@
+# int() function
+
+# Example of calling the int() function
+user_age = int("99")
+
+# Name: int
+# Arguments: A thing to be converted to an integer
+# Side Effects: None
+# Return Value: An integer 
+
+
+
+
+# str() function
+
+# Example of calling the str() function
+user_age = str(100)
+
+# Name: str
+# Arguments: A thing to be converted to a string
+# Side Effects: None
+# Return Value: A string 
+
+
+
+
+# bool() function
+
+# Example of calling the bool() function
+
+is_true = bool(1)
+
+# Name: bool
+# Arguments: A thing to be evaluated for its truthiness
+# Side Effects: None
+# Return Value: True or False
+
+
+
+# float() function
+
+# Example of calling the float() function
+my_float = float("2.78")
+
+# Name: float
+# Arguments: A thing to be converted to a floating-point number
+# Side Effects: None
+# Return Value: A floating-point number 
+
+
+
+
+# len() function
+
+# Example of calling the len() function
+word_length = len("Hahhhhhhh")
+
+# Name: len
+# Arguments: The thing you want the length of
+# Side Effects: None
+# Return Value: The length of the argument
+
+
+
+
+# pow() function
+
+# Example of calling the pow() function
+result = pow(2, 3)
+
+# Name: pow
+# First Argument: Base
+# Second Argument: Exponent
+# Side Effects: None
+# Return Value: The first argument to the power of the second argument
+
+
+
+
+
+
+# abs() function
+
+# Example of calling the abs() function
+positive_value = abs(-15)
+
+# Name: abs
+# Arguments: A number
+# Side Effects: None
+# Return Value: The absolute value of the given number
