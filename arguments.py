414e0c46ca1f04e688ad086a1abb1d3a7f7f8cd9
WeiW389 <w.wei.music@gmail.com>
2026-03-03 21:36:01 -0500

    Create arguments.py

---
+++ arguments.py
@@ -0,0 +1,24 @@
+#1
+print(1,2,3,4,5,6,7,8,9,10)
+
+#2
+print()
+#it gives me an empty line
+
+#3
+#it gives me a TypeError: pow() missing required argument 'exp'
+
+#4
+#round
+
+#5
+#it is being called
+
+#6
+#2 arguments
+
+#7
+#3.14158 and 2
+
+#8
+#it has equal sign, so it returns a value but doesn't show it in the terminal unless i write print(result) to call it
